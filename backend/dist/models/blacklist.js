3f1b844e7605b5b7e9c3a18be941b14fbdc8a452
wolverine0606 <dimaspotapov0606@gmail.com>
2024-05-16 16:06:08 +0200

    aaa

--- backend/dist/models/blacklist.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const mongoose_1 = require("mongoose");
-const blacklist = new mongoose_1.Schema({
-    email: { type: String, unique: true, required: true },
-});
-const blacklistModel = (0, mongoose_1.model)("blacklist", blacklist);
-exports.default = blacklistModel;
--- backend/dist/utils/helper.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.sendErrorRes = void 0;
-const sendErrorRes = (res, message, statusCode) => {
-    return res.status(statusCode).json({ message });
-};
-exports.sendErrorRes = sendErrorRes;
